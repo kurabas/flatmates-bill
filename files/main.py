fb993592f1321a4b27a68a018db40f2661f351c8
S. Kurabachew <surafel.kurabachew@hva.nl>
2024-11-25 20:49:15 -0500

    added flatmates bill pdf

--- files/main.py
+++ files/main.py
@@ -1,3 +1,6 @@
+from fpdf import FPDF
+
+
 class Bill:
 
     def __init__(self, amount, period):
@@ -22,12 +25,43 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatname1, flatname2, bill):
-        pass
+        flatmate1_pay = str(round(flatname1.pays(bill, flatname2), 2))
+        flatmate2_pay = str(round(flatname2.pays(bill, flatname1), 2))
+
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # House icon
+        pdf.image("house.png", w=30, h=30)
+
+        # Title
+        pdf.set_font(family="Times", size=24, style="B")
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align="C", ln=1)
+
+        # Period label
+        pdf.set_font(family="Times", size=14, style="B")
+        pdf.cell(w=100, h=40, txt="Period", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        # Name and due amount of first flatmate
+        pdf.set_font(family="Times", size=12)
+        pdf.cell(w=100, h=25, txt=flatname1.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate1_pay, border=0, ln=1)
+
+        # Name and due amount of first flatmate
+        pdf.set_font(family="Times", size=12)
+        pdf.cell(w=100, h=25, txt=flatname2.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate2_pay, border=0, ln=1)
+
+        pdf.output(self.filename)
 
 
-the_bill = Bill(120, "March 2021")
+the_bill = Bill(120, "April 2021")
 john = Flatmate("John", 20)
 marry = Flatmate("Marry", 25)
 
-print("John pays: ", john.pays(the_bill, flatmate2=marry))
-print("Marry pays: ",marry.pays(the_bill, flatmate2=john))
+print("John pays: ", john.pays(bill=the_bill, flatmate2=marry))
+print("Marry pays: ", marry.pays(bill=the_bill, flatmate2=john))
+
+pdf_report = PdfReport(filename="Report1.pdf")
+pdf_report.generate(flatname1=john, flatname2=marry, bill=the_bill)
