ce679bac68c482d24f848040e1a3715a3bb6b0c4
S. Kurabachew <surafel.kurabachew@hva.nl>
2024-11-28 15:30:01 -0500

    webbrowser open pdf added

--- files/main.py
+++ files/main.py
@@ -1,3 +1,6 @@
+import os.path
+import webbrowser
+
 from fpdf import FPDF
 
 
@@ -54,6 +57,8 @@ class PdfReport:
         pdf.cell(w=150, h=25, txt=flatmate2_pay, border=0, ln=1)
 
         pdf.output(self.filename)
+
+        webbrowser.open('file://' + os.path.realpath(self.filename))
 
 
 the_bill = Bill(120, "April 2021")
